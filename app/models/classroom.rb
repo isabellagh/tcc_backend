d94ad11030316a6f8c938f5a23729dd69df55885
isabellagh <isabellagh88@gmail.com>
2022-07-17 10:10:07 -0400

    migration fixed

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,5 +1,5 @@
 class Classroom < ApplicationRecord
-    has_many :children
+    has_many :children, dependent: :destroy
     validates_presence_of :room_name, :age
     validates_uniqueness_of :room_name, message: "name must be unique"
 
